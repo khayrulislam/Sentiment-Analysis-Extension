bdd439bc3c7a4fecfca9f86616f09d9e0f8c96c7
khayrulislam <bsse0822@iit.du.ac.bd>
2019-07-17 10:03:45 +0600

    update content file

--- content.js
+++ content.js
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with Reddit Hover Text. If not, see <http://www.gnu.org/licenses/>.
  **/
-
 /**
  * The timeout variable we use to delay showing and hiding of the hover div.
  **/
